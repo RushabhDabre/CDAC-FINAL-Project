5f74ad05e671cd33cf6bdc337a0f1514f0108f79
Rushabh Dabre <rushabhdabare14@gmail.com>
2024-02-06 21:47:41 +0530

    Added Ui Components

--- trackflow/src/App.js
+++ trackflow/src/App.js
@@ -1,12 +1,18 @@
 import { React } from 'react';
 import {Route, Routes} from 'react-router-dom';
 import CreateProject from './Components/CreateProject';
+import AddEmployee from './Components/AddEmployee';
+import CreateTask from './Components/CreateTask';
+import './App.css';
+import TaskList from './Components/TaskList';
 
 function App() {
   return (
-    <div>
+    <div className='App-header'>  
+      <TaskList/>    
       <Routes>
-        <Route path="/" element={<CreateProject/>}/>
+        {/* <Route path="/" element={<CreateTask/>}/> */}
+        {/* <Route path="/" element={<CreateProject/>}/> */}
       </Routes>
     </div>
   );
--- trackflow/src/Components/CreateProject.js
+++ trackflow/src/Components/CreateProject.js
@@ -6,28 +6,42 @@ export default function CreateProject() {
     const ref = useRef(null) //used for Loading Bar
     let navigate = useNavigate();
 
-  return (
+  return ( 
         <div className="container d-flex justify-content-center ">
-            <LoadingBar color="#f11946" ref={ref} shadow={true} />
-            <div className="shadow-lg p-4 m-5" style={{"width": '25rem'}}>
-                <h1 className="d-flex justify-content-center text-success mb-3">Create Project</h1>      
-                <form >
-                    <div className="mb-3">   
-                        <input  type="text" placeholder="Text" className="form-control" />
-                    </div>
-                    <div className="mb-3">   
-                        <input  type="text" placeholder="Text" className="form-control" />
-                    </div>
-                    <div className="row g-3 align-items-center d-flex justify-content-center mb-3">
-                        <div className="col-auto ">
-                            <button type="submit" className="btn btn-success w-100 font-weight-bold mt-2" >Create Project</button>
+            <LoadingBar color="#f11946" ref={ref} shadow={true} /> 
+            <div className='container d-flex justify-content-center'>
+                <div className=" shadow-lg p-4 m-5" style={{"width": '40rem'}}>
+                    <h5 className="d-flex mb-4"><b>Add New Project</b></h5>      
+                    <form >
+                        <div className="mb-3">   
+                            <label className='text-muted'><h6>Project Title</h6></label>
+                            <input  type="text" placeholder="e.g. TrackFlow - Task Progress Manager" className="form-control" />
                         </div>
-                        <div className="col-auto">
-                            <button type="button" className="btn btn-secondary w-100 font-weight-bold mt-2" on>Cancel</button>
+                        <div className="mb-3">   
+                            <label className='text-muted'><h6>Description</h6></label>
+                            <textarea placeholder="e.g. Create a Task Progress Manager system for......" rows="8" className="form-control" />
                         </div>
-                    </div>
-                </form>    
-            </div> 
+                        <div className="mb-3">   
+                            <label className='text-muted'><h6>Techstack</h6></label>
+                            <input  type="text" placeholder="e.g. MERN Stack, Mysql for db,......." className="form-control" />
+                        </div>
+                        <div className="mb-3">   
+                            <label className='text-muted'><h6>Deadline</h6></label>
+                            <input  type="date" className="form-control" />
+                        </div>
+                        {/* Project Assign to remainig..... */}
+                        
+                        <div className="row g-3 align-items-center d-flex justify-content-center mb-3">
+                            <div className="col-auto ">
+                                <button type="submit" className="btn btn-success w-100 font-weight-bold mt-2" >Create Project</button>
+                            </div>
+                            <div className="col-auto">
+                                <button type="button" className="btn btn-secondary w-100 font-weight-bold mt-2" onClick={()=>{ref.current.complete(); setTimeout(() => navigate("/"), 500);}}>Cancel</button>
+                            </div>
+                        </div>
+                    </form>    
+                </div> 
+            </div>           
         </div>
   )
 }
