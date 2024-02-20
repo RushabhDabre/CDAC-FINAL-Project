1ba6c99db535e55be6d125b59ef2deae9875fb4d
ADITYA SHASTRAKAR <adityashastrakar123@gmail.com>
2024-02-20 16:49:36 +0530

    Added DotNet API

---
+++ DotNetApi/DotNetWebApi/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetWebApi.Models
+{
+    public partial class Task
+    {
+        public int Tid { get; set; }
+        public string Tname { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateOnly? Assigneddate { get; set; }
+        public DateOnly? Deadline { get; set; }
+        public ulong? Status { get; set; }
+        public int? Progress { get; set; }
+        public int? Empid { get; set; }
+        public int? Pid { get; set; }
+
+        public virtual Employee? Emp { get; set; }
+        public virtual Project? PidNavigation { get; set; }
+    }
+}
---
+++ DotNetApi/DotNetWebApi/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetWebApi.Models
+{
+    public partial class Team
+    {
+        public int Teamid { get; set; }
+        public int Pid { get; set; }
+        public int Empid { get; set; }
+        public string? Comments { get; set; }
+        public DateOnly? AssignedDate { get; set; }
+        public ulong? Status { get; set; }
+        public DateOnly? Releasedate { get; set; }
+
+        public virtual Employee Emp { get; set; } = null!;
+        public virtual Project PidNavigation { get; set; } = null!;
+    }
+}
