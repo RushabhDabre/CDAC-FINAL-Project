dc64ebba94615f272b018f29e796eb00743941c4
ADITYA SHASTRAKAR <adityashastrakar123@gmail.com>
2024-02-20 20:09:38 +0530

    Task Add using Dot Net API

---
+++ DotNetApi/DotNetApi/Controllers/TasksController.cs
@@ -0,0 +1,35 @@
+﻿using DotNetApi.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetApi.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public List<Tasktable> GetTasktables()
+        {
+            List<Tasktable> list = new List<Tasktable>();
+            using(var db = new trackflowdbContext())
+            {
+                list = db.Tasktables.ToList();
+            }
+            return list;
+        }
+
+        [HttpPost]
+        public Tasktable AddTasks(Tasktable tasktable)
+        {
+            using(var db = new trackflowdbContext())
+            {
+                db.Tasktables.Add(tasktable);
+                db.SaveChanges();
+            }
+            return tasktable;
+        }
+    }
+}
---
+++ DotNetApi/DotNetApi/Models/Tasktable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetApi.Models
+{
+    public partial class Tasktable
+    {
+        public int Tid { get; set; }
+        public string Tname { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime? Assigneddate { get; set; }
+        public DateTime? Deadline { get; set; }
+        public bool? Status { get; set; }
+        public int? Progress { get; set; }
+        public int? Empid { get; set; }
+        public int? Pid { get; set; }
+
+        public virtual Employee? Emp { get; set; }
+        public virtual Project? PidNavigation { get; set; }
+    }
+}
